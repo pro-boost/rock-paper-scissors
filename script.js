6d5f44bc3579e9fb181e600a76cee8ccf8ee90d0
MedBrk <150671350+med-ctrl@users.noreply.github.com>
2024-04-09 15:32:43 +0000

    Enhance UI: - Add a background picture to improve aesthetics. - Style elements for better visual appeal. - Utilize Bootstrap classes dynamically in JavaScript for buttons. - Remove unnecessary code for cleaner implementation.

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-
-let result;
-let computersChoice;
 let rock = document.querySelector("#rock");
 let paper = document.querySelector("#paper");
 let scissors = document.querySelector("#scissors");
 let playerChoice;
+let computersChoice;
+let result;
 
 
 //function to chose a random weapon (Rock,Paper or Scissors)
@@ -14,31 +13,32 @@ function randomWeapon(){
     return ranWeapon;
 }
 
-
-// Add event listeners to buttons
+//-----------------------------------------------
 rock.addEventListener("click", function() {
     playerChoice = "Rock";
-    rock.style.backgroundColor="green";
-    paper.style.backgroundColor="";
-    scissors.style.backgroundColor="";
+    rock.classList.add("btn", "btn-success");
+    paper.classList.remove("btn-success");
+    scissors.classList.remove("btn-success");
     playGame();
 });
 
 paper.addEventListener("click", function() {
     playerChoice = "Paper";
-    rock.style.backgroundColor="";
-    paper.style.backgroundColor="green";
-    scissors.style.backgroundColor="";
+    rock.classList.remove("btn-success");
+    paper.classList.add("btn", "btn-success") ;
+    scissors.classList.remove("btn-success");
     playGame();
 });
 
 scissors.addEventListener("click", function() {
     playerChoice = "Scissors";
-    rock.style.backgroundColor="";
-    paper.style.backgroundColor="";
-    scissors.style.backgroundColor="green";
+    rock.classList.remove("btn-success");
+    paper.classList.remove("btn-success");
+    scissors.classList.add("btn", "btn-success");
+    
     playGame();
 });
+
 
 //function to compare the randomWeapon with playersChoice
 function playGame(){
@@ -46,17 +46,23 @@ function playGame(){
     computersChoice=randomWeapon();
     if(playerChoice === computersChoice){
         result = "Draw! Both chose " + playerChoice + ".";
+        document.querySelector("#result").style.color="black";
     }
+
     if ((playerChoice==="Rock"&& computersChoice==="Paper") 
         || (playerChoice==="Paper"&& computersChoice==="Scissors") 
         || (playerChoice==="Scissors"&& computersChoice==="Rock")) {
             result = "You loose! " +computersChoice + " beats " + playerChoice + ".";
+            document.querySelector("#result").style.color="red";
         }
+
     if ((playerChoice==="Rock"&& computersChoice==="Scissors") 
         || (playerChoice==="Paper"&& computersChoice==="Rock") 
         || (playerChoice==="Scissors"&& computersChoice==="Paper")) {
             result = "You Win! " + playerChoice + " beats " + computersChoice + ".";
+            document.querySelector("#result").style.color="#28a745";
         }
-        document.querySelector("#computersChoice").textContent = "The computer's choice is " +computersChoice;
-        document.querySelector("#result").textContent = result;
+
+    document.querySelector("#computersChoice").textContent = "The computer's choice was " +computersChoice;
+    document.querySelector("#result").textContent = result;
 }
